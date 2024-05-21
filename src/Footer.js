d365b0acf7b4c6ef2c78d8b112bc3c52388ee4cb
Camilla C <154556489+hicamilla@users.noreply.github.com>
2024-05-21 15:59:17 +0200

    # 2 Search engine

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return <p className="App-footer"> Created with ☕ by Camilla. It's available as open-source on <a href="https://github.com/hicamilla/new-dictionary">GitHub</a> </p>
+}
+
+
