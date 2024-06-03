bed1184f5da51c7deeb0f83f87c76d0a7a9a7ff4
Camilla C <154556489+hicamilla@users.noreply.github.com>
2024-06-03 15:30:06 +0200

    #8 phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,10 +5,11 @@ export default function Phonetic(props) {
     <div className="Phonetic">
       <h2>{props.phonetic.text}</h2>
       <br />
-      <a href="{props.phonetic.audio}" target="_blank" rel="noreferrer">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      
+      <br />
+      <span>{props.phonetic.text}</span>
     </div>
-  )
+  );
 }
